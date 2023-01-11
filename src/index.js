77ffda4ad1f816c681c7476ec3cb9a6f02dd056e
zislamtanvir <zislamtanvir@gmail.com>
2023-01-11 12:27:12 +0600

    Add h1,nav in header section

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import _ from 'lodash';
+import _ from "lodash";
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement("div");
 
-    // Lodash, now imported by this script
-    const head1 = document.getElementById("content");
-
+  // Lodash, now imported by this script
+  const head1 = document.getElementById("content");
+  console.log(head1);
+  return element;
+}
+function nav() {
+  function createHtmlElement(elementType, text) {
+    const element = document.createElement(elementType);
+    if (text != "") {
+      element.textContent = text;
+    }
     return element;
+  }
+  const divEl = createHtmlElement("div", " ");
+  const navEl = document.createElement("nav");
+  const ulEl = document.createElement("ul");
+
+  const h1El = createHtmlElement("h2", "Header 1");
+  divEl.appendChild(h1El);
+
+  const navText = ["Home", "Menu", "Content"];
+  for (let i = 0; i < 3; i++) {
+    const liEl = document.createElement("li");
+    liEl.textContent = navText[i];
+    ulEl.appendChild(liEl);
+  }
+  console.log(ulEl);
+  return divEl;
 }
 
 document.body.appendChild(component());
+document.body.appendChild(nav());
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,25 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Food-on-a-plate',
-            templateContent: `
-            <html>
-              <body>
-                <div id="content"></div>
-              </body>
-            </html>
-          `
-        }),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Food Resturant",
+      template: "src/template.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
 };
